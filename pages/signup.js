c5e3ac2af76ffe654a990337fc1cfcbfe024afc3
1mmoneylog-sudo <1mmoneylog@gmail.com>
2026-09-03 11:51:02 +0900

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -1,1 +1,11 @@
+import Link from "next/link";
 
+export default function Signup() {
+  return (
+    <div className="tool-hero-inner" style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h1>회원가입</h1>
+      <p>준비 중입니다. 곧 만나요!</p>
+      <Link href="/">← 메인으로 돌아가기</Link>
+    </div>
+  );
+}
