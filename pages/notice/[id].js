0daa29c7fe8d19012495a81284ba8c832e0ad4eb
1mmoneylog-sudo <1mmoneylog@gmail.com>
2026-09-08 14:22:45 +0900

    Update [id].js

--- pages/notice/[id].js
+++ pages/notice/[id].js
@@ -234,11 +234,11 @@ export default function NoticeDetail({ notice }) {
                   
                   <div className="image-slider-track" id="img-scroll-box">
                     {notice.image_urls.map((img, i) => {
-                      const rawUrl = img?.url ?? '';
-                      const fullUrl = rawUrl.startsWith('http')
-                        ? rawUrl
-                        : `https://apply.lh.or.kr/${rawUrl.replace(/^\//, '')}`;
-
+                     const rawUrl = img?.url ?? '';
+                      const cleanedUrl = rawUrl.replace(/^http:\/\//i, 'https://');
+                      const fullUrl = cleanedUrl.startsWith('http')
+                        ? cleanedUrl
+                        : `https://apply.lh.or.kr/${cleanedUrl.replace(/^\//, '')}`;
                       return (
                         <div key={`i${i}`} className="image-slide-item">
                           <img 
