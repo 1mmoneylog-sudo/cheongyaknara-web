6540cbdb7db7f79c67d2513e1f2678a944d3b281
1mmoneylog-sudo <1mmoneylog@gmail.com>
2026-09-11 15:58:17 +0900

    Update mypage.js

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";ㄴ
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useUser } from "../lib/useUser";
