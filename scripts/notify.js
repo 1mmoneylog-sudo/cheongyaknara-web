bba2aa4a2f42f09f51a6b6d812c8800546f8ac56
1mmoneylog-sudo <1mmoneylog@gmail.com>
2026-09-16 11:26:37 +0900

    Update notify.js

--- scripts/notify.js
+++ scripts/notify.js
@@ -12,7 +12,10 @@ const PREV_NOTICES_PATH = path.join(__dirname, "..", "data", "notices.prev.json"
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    realtime: { transport: ws },
+  }
 );
 const resend = new Resend(process.env.RESEND_API_KEY);
 
